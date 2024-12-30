98102361466511b25a01018c3b8334af87bc6c88
shailendravermaa <sv8576318@gmail.com>
2024-12-30 19:11:56 +0530

    fixed some about the app express varible

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')//common js and module js
 
 
-const app = express()
+const app = express()//app is very powerfull variable
 
 const port = 3000//virtual port bhi hote hai ,as defined by the 16-bit field in the TCP and UDP headers. 2^(16)=65,536 range (0-65,535) for the network itself
 //made a server for which 
@@ -20,10 +20,10 @@ app.get('/twitter',(req,res)=>{// slash par listen karta hai ans slash twitter p
 app.get('/login',(req,res)=>{//this is all four parameter
   res.send(' <h1>please login </h1>')
 })
-app.get('/youtube',(req,res)=>{
+app.get('/youtube',(req,res)=>{//app give the power to send 
     res.send('<h2>chai aur code</h2>')
 })
-  //listen jo port me hai
+  //listen jo port me hai //we have made a server that listen on / , /twitter ,/login, /youtube
 app.listen(process.env.PORT, () => {//we are listing to the 3000 port 
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
